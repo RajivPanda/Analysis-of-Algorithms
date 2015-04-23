6d1a0c116c8dedc62dfb47ffd333294bf63d9abb
Rajiv Lochan Panda <rajivlochanp@gmail.com>
2015-04-23 20:44:32 +0530

    Updated MinMax.C
    Implementation of Min-max by divide and conquer as the previous method was considered as brute-force method.

--- MinMax.C
+++ MinMax.C
@@ -1,24 +1,48 @@
 #include<stdio.h>
 #include<conio.h>
+void minmax(int a[],int lb,int ub,int *min,int *max)
+{
+int mid,min1,max1;
+if(lb==ub)
+	*min=*max=a[lb];
+else if(lb==ub-1)
+		if(a[lb]>a[ub])
+			{
+			*max=a[lb];
+			*min=a[ub];
+			}
+		else
+			{
+			*max=a[ub];
+			*min=a[lb];
+			}
+      else
+	       {
+	       mid=(lb+ub)/2;
+	       minmax(a,lb,mid, min,max);
+	       minmax(a,mid+1,ub,&min1,&max1);
+	       if(max1>*max)
+			*max=max1;
+	       if(min1<*min)
+			*min=min1;
+	       }
+
+}
+
 void main()
 {
-	int i,min,max,num,arr[100];
-	clrscr();
-	printf("Enter the size of the array:");
-	scanf("%d",&num);
-	printf("Enter the Elements into the array:");
-	for(i=0; i<num; i++)
-		scanf("%d",&arr[i]);
-	min=max=arr[0];
-	for(i=0; i<num; i++)
-	{
-		if(arr[i]<min)
-			min=arr[i];
-		if(arr[i]>max)
-			max=arr[i];
-	}
-	printf("\n%d is minimum \n%d is maximum",min,max);
-	getch();
+int n,a[50],min,max,lb,ub;
+clrscr();
+printf("\nEnter the size of the array : ");
+scanf("%d",&n);
+printf("\nEnter the elements into the array: \n");
+for(lb=0;lb<n;lb++)
+	scanf("%d",&a[lb]);
+lb=0;  //lower bound of the array
+ub=n-1;   //upper bound of the array
+minmax(a,lb,ub,&min,&max);
+printf("\n%d is minimum \n%d is maximum",min,max);
+getch();
 }
 /*
 OUTPUT:
